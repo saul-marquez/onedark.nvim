40abf786540fd125d565aa553fa9655107dda77a
Saul Marquez <saul.marquez@mdwpartners.com>
2024-05-21 16:28:45 +0200

    My palette

--- lua/lualine/themes/onedark.lua
+++ lua/lualine/themes/onedark.lua
@@ -1,7 +1,6 @@
 local c = require('onedark.colors')
-local cfg = vim.g.onedark_config
 local colors = {
-    bg = c.bg0,
+    bg = c.bg1,
     fg = c.fg,
     red = c.red,
     green = c.green,
@@ -14,19 +13,46 @@ local colors = {
 
 local one_dark = {
     inactive = {
-        a = {fg = colors.gray, bg = colors.bg, gui = 'bold'},
-        b = {fg = colors.gray, bg = colors.bg},
-        c = {fg = colors.gray, bg = cfg.lualine.transparent and c.none or c.bg1},
+        a = { fg = colors.gray, bg = colors.bg, gui = 'bold' },
+        b = { fg = colors.gray, bg = colors.bg },
+        c = { fg = colors.gray },
+        x = { fg = colors.gray },
     },
     normal = {
-        a = {fg = colors.bg, bg = colors.green, gui = 'bold'},
-        b = {fg = colors.fg, bg = c.bg3},
-        c = {fg = colors.fg, bg = cfg.lualine.transparent and c.none or c.bg1},
+        a = { fg = colors.bg, bg = colors.green, gui = 'bold' },
+        b = { fg = colors.gray },
+        c = { fg = colors.green },
+        x = { fg = colors.gray },
     },
-    visual = {a = {fg = colors.bg, bg = colors.purple, gui = 'bold'}},
-    replace = {a = {fg = colors.bg, bg = colors.red, gui = 'bold'}},
-    insert = {a = {fg = colors.bg, bg = colors.blue, gui = 'bold'}},
-    command = {a = {fg = colors.bg, bg = colors.yellow, gui = 'bold'}},
-    terminal = {a = {fg = colors.bg, bg = colors.cyan, gui = 'bold'}},
+    visual = {
+        a = { fg = colors.bg, bg = colors.purple, gui = 'bold' },
+        b = { fg = colors.gray },
+        c = { fg = colors.purple },
+        x = { fg = colors.gray },
+    },
+    replace = {
+        a = { fg = colors.bg, bg = colors.red, gui = 'bold' },
+        b = { fg = colors.gray },
+        c = { fg = colors.red },
+        x = { fg = colors.gray },
+    },
+    insert = {
+        a = { fg = colors.bg, bg = colors.blue, gui = 'bold' },
+        b = { fg = colors.gray },
+        c = { fg = colors.blue },
+        x = { fg = colors.gray },
+    },
+    command = {
+        a = { fg = colors.bg, bg = colors.yellow, gui = 'bold' },
+        b = { fg = colors.gray },
+        c = { fg = colors.yellow },
+        x = { fg = colors.gray },
+    },
+    terminal = {
+        a = { fg = colors.bg, bg = colors.cyan, gui = 'bold' },
+        b = { fg = colors.gray },
+        c = { fg = colors.cyan },
+        x = { fg = colors.gray },
+    },
 }
 return one_dark;
